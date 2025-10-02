54a0bc565b8af31e211eca83221ae0002fb8c88b
A.Galimzhan <a.galimzhan@redmadrobot.kz>
2025-10-02 22:43:30 +0500

    Feat: Solve the problem 617A on Codeforces

---
+++ cp/codeforces/617A.cpp
@@ -0,0 +1,33 @@
+/*
+An elephant decided to visit his friend. It turned out that the elephant's house is located at point 0 and his friend's house is located at point x(x > 0) of the coordinate line. In one step the elephant can move 1, 2, 3, 4 or 5 positions forward. Determine, what is the minimum number of steps he need to make in order to get to his friend's house.
+Input
+
+The first line of the input contains an integer x (1 ≤ x ≤ 1 000 000) — The coordinate of the friend's house.
+Output
+
+Print the minimum number of steps that elephant needs to make to get from point 0 to point x.
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    int y = 0;
+    cin >> y;
+    int ans = 0;
+    
+    for (int i = 5; i >= 1; i--) {
+        if (y == 0) {
+            break;
+        }
+        if (y / i >= 1) {
+            y = y - i;
+            i++;
+            ans++;
+        }
+    }
+
+    cout << ans;
+    return 0;
+}
